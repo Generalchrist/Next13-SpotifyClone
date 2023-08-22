b33fb75575d48ac19f9e86b55cd4e92d73929c28
Mert patlar <meertpatlar@gmail.com>
2023-08-23 01:14:47 +0300

    useUser hook and userProvider added

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps{
+    children : React.ReactNode
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({children}) => (
+    <MyUserContextProvider>
+        {children}
+    </MyUserContextProvider>
+)
+
+export default UserProvider
